4240a1a738c33f353e389777ebce9d2576c381d7
Antony Ademefun <antonyademefun@gmail.com>
2019-06-26 22:32:44 +0100

    Added Login Page

---
+++ app/src/main/java/com/e/globed/ui/main/LoginPageViewModel.java
@@ -0,0 +1,7 @@
+package com.e.globed.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginPageViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/e/globed/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/e/globed/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.e.globed.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,  R.string.tab_text_2,  R.string.tab_text_2};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -27,10 +27,20 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new UserPage();
+                break;
+            default:
+                PlaceholderFragment.newInstance(position + 1);
+                break;
+        }
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        return fragment;
     }
+
 
     @Nullable
     @Override
@@ -38,9 +48,10 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         return mContext.getResources().getString(TAB_TITLES[position]);
     }
 
+
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 2;
+        return 3;
     }
 }
